f3535e2f7a99500a83de5e7bfc42a9c4946baae2
Patrick Seebacher <patrick.seebacher@gmail.com>
2022-10-02 12:10:44 +0200

    TextMeshPro Preview Package added

--- Assets/Scripts/UIMainMenu.cs
+++ Assets/Scripts/UIMainMenu.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class UIMainMenu : MonoBehaviour
 {
+    public Transform MouseLight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (MouseLight == null) MouseLight = GetComponentInChildren<Light2D>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveMouseLight();
+    }
+
+    void MoveMouseLight()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
+        MouseLight.position = new Vector3(mousePosition.x, mousePosition.y, MouseLight.position.z);
     }
 
     public void ButtonStartClick()
